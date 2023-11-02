9403aad5697b0c740e8dec39d3ccd28017f15487
kumartanmay <ktanmay@gmail.com>
2023-11-02 23:34:52 +0000

    checking for more test cases where number ends or starts with zero

--- LS/JS110/JS110-JS119/Revision/Ex-easy-4/palindromNumbers.js
+++ LS/JS110/JS110-JS119/Revision/Ex-easy-4/palindromNumbers.js
@@ -44,3 +44,5 @@ console.log(isPalindromicNumber(34543));        // true
 console.log(isPalindromicNumber(123210));       // false
 console.log(isPalindromicNumber(22));           // true
 console.log(isPalindromicNumber(5));            // true
+console.log(isPalindromicNumber(500));
+console.log(isPalindromicNumber(007));
