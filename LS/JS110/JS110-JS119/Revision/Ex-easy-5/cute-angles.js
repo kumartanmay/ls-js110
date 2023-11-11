51683d6c368caa328113ecc17d7ccd81d4145029
kumartanmay <ktanmay@gmail.com>
2023-11-11 12:56:55 +0000

    refactored the code to take negative floating points as input

--- LS/JS110/JS110-JS119/Revision/Ex-easy-5/cute-angles.js
+++ LS/JS110/JS110-JS119/Revision/Ex-easy-5/cute-angles.js
@@ -32,15 +32,23 @@ algo:
 */
 
 function dms(angle) {
-    let deg = Math.floor(angle);
-    let minutes = (angle - deg) * 60;
-    let seconds = (minutes - Math.floor(minutes)) * 60;
-    
-    console.log(`${deg}˚${Math.floor(minutes)}'${Math.floor(seconds)}"`);
+    let deg = 0;
+    let minutes = 0;
+    let seconds = 0;
+    if(angle >= 0) {
+        deg = Math.floor(angle);
+        minutes = (angle - deg) * 60;
+        seconds = (minutes - Math.floor(minutes)) * 60;
+        console.log(`${deg}˚${Math.floor(minutes)}'${Math.floor(seconds)}"`);
+    } else {
+        angle = 360 + angle;
+        dms(angle);
+    }
 }
-dms(30);           // 30°00'00"
+dms(-30);           // 30°00'00"
 dms(76.73);        // 76°43'48"
 dms(254.6);        // 254°35'59"
 dms(93.034773);    // 93°02'05"
 dms(0);            // 0°00'00"
 dms(360);          // 360°00'00" or 0°00'00"
+dms(-420);  
