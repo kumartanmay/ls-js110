ef75bfb2c3b55334bf34fd16b764a988bf66f5d2
kumartanmay <ktanmay@gmail.com>
2023-11-19 04:59:32 +0000

    fished out objects from an array

---
+++ LS/JS110/JS110-JS119/Revision/list-processing/9-inventory.js
@@ -0,0 +1,56 @@
+/*
+given an array of objects containing the transaction details as an object. 
+Return an array containing transactions with the specified transaction id.
+
+assume: 
+transaction id is always valid 
+
+example
+let transactions = [ { id: 101, movement: 'in',  quantity:  5 },
+                     { id: 105, movement: 'in',  quantity: 10 },
+                     { id: 102, movement: 'out', quantity: 17 },
+                     { id: 101, movement: 'in',  quantity: 12 },
+                     { id: 103, movement: 'out', quantity: 20 },
+                     { id: 102, movement: 'out', quantity: 15 },
+                     { id: 105, movement: 'in',  quantity: 25 },
+                     { id: 101, movement: 'out', quantity: 18 },
+                     { id: 102, movement: 'in',  quantity: 22 },
+                     { id: 103, movement: 'out', quantity: 15 }, ];
+
+transactionsFor(101, transactions);
+// returns
+// [ { id: 101, movement: "in",  quantity:  5 },
+//   { id: 101, movement: "in",  quantity: 12 },
+//   { id: 101, movement: "out", quantity: 18 }, ]
+
+ds:
+==
+input: number, array
+output: array
+
+algo:
+====
+1. iterate over each element of the array
+2. if the element contains the transaction id, push the element into the resulting array
+*/
+let transactions = [ { id: 101, movement: 'in',  quantity:  5 },
+                     { id: 105, movement: 'in',  quantity: 10 },
+                     { id: 102, movement: 'out', quantity: 17 },
+                     { id: 101, movement: 'in',  quantity: 12 },
+                     { id: 103, movement: 'out', quantity: 20 },
+                     { id: 102, movement: 'out', quantity: 15 },
+                     { id: 105, movement: 'in',  quantity: 25 },
+                     { id: 101, movement: 'out', quantity: 18 },
+                     { id: 102, movement: 'in',  quantity: 22 },
+                     { id: 103, movement: 'out', quantity: 15 }, ];
+
+function transactionsFor(num, arr) {
+    let result = [];
+    arr.forEach(elem => {
+        if(elem['id'] === num) result.push(elem);
+    })
+    
+    return result;
+}
+
+console.log(transactionsFor(101, transactions));
