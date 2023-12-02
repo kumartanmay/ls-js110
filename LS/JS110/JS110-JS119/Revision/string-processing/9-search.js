6662e31e19ef8f5bd22bc2732a0035a9932bc59f
kumartanmay <ktanmay@gmail.com>
2023-12-02 04:13:55 +0000

    used filter function to find the matching word and then counted the length of the filtered array

---
+++ LS/JS110/JS110-JS119/Revision/string-processing/9-search.js
@@ -0,0 +1,34 @@
+/*
+given a sentence, find the frequency of the word provided. Word is case insensitive.
+
+ex:
+==
+
+const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
+
+searchWord('sed', text);      // 3
+
+ds:
+===
+input: string
+output: number
+
+other vars:
+===========
+an array holding the words in the string
+
+algo
+====
+1. split the string into an array of words
+2. iterate over the list of words and match if it contains the word
+*/
+
+function searchWord(word, sentence) {
+    return sentence.split(" ")
+            .filter(elem => elem.toLowerCase() === word).length;
+    // return sentence.match(/word/gi).length;
+}
+
+const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
+
+console.log(searchWord('sed', text));      // 3
