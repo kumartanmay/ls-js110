eeecd76f14bf5c75a7a20ccee678d383def67540
kumartanmay <ktanmay@gmail.com>
2023-12-02 03:58:41 +0000

    split words by a whitespace and not empty string

---
+++ LS/JS110/JS110-JS119/Revision/string-processing/8-how-long.js
@@ -0,0 +1,57 @@
+/*
+given a string, return an array consisting of the words and its length as elements.
+
+ex:
+==
+wordLengths('cow sheep chicken');
+// ["cow 3", "sheep 5", "chicken 7"]
+
+wordLengths('baseball hot dogs and apple pie');
+// ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+wordLengths("It ain't easy, is it?");
+// ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+wordLengths('Supercalifragilisticexpialidocious');
+// ["Supercalifragilisticexpialidocious 34"]
+
+wordLengths('');      // []
+wordLengths();        // []
+
+ds:
+===
+input: string
+output: array
+
+other var:
+an array holding words of the string
+
+algo:
+====
+1. Split the string into constituent words
+2. Concatenate each element of the array with the length of the element
+3. return an empty array in case of empty string
+*/
+
+function wordLengths(string) {
+    let result = [];
+    if(string) {
+        return string.split(' ').map(word => word + ' ' + word.length)
+    }
+    return result;
+}
+
+console.log(wordLengths('cow sheep chicken'));
+// ["cow 3", "sheep 5", "chicken 7"]
+
+console.log(wordLengths('baseball hot dogs and apple pie'));
+// ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+console.log(wordLengths("It ain't easy, is it?"));
+// ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+console.log(wordLengths('Supercalifragilisticexpialidocious'));
+// ["Supercalifragilisticexpialidocious 34"]
+
+console.log(wordLengths(''));      // []
+console.log(wordLengths());        // []
