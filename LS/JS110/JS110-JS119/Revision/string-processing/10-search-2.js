548d65f0df9e9feb9fc9bcc2a67e752c6dbd542e
kumartanmay <ktanmay@gmail.com>
2023-12-02 04:26:40 +0000

    used map method to find the matching string and return a new string by highlighting the matching string

---
+++ LS/JS110/JS110-JS119/Revision/string-processing/10-search-2.js
@@ -0,0 +1,34 @@
+/*
+given a sentence, find the word from the sentence and highlight it by putting it between **
+and changing it to upper case. Return the modified sentence.
+
+ds:
+input: string
+output: string
+
+other vars
+array holding the words of the original string
+array holding the words of the modified string
+
+algo:
+====
+1. split the sentence into words
+2. find the matching words and convert it into uppercase and place it between **
+3. join the resulting array into a string
+*/
+
+function search(word, sentence) {
+    return sentence.split(" ")
+                    .map(elem => {
+                        if (elem.toLowerCase() === word) return `*${word.toUpperCase()}*`
+                        else return elem;
+                    })
+                    .join(" ");
+}
+
+const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Sed quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur blasedbla?';
+
+console.log(search('sed', text));
+console.log(search('qui', text));
+// returns
+// "**SED** ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, **SED** quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, **SED** quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? **SED** quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur blasedbla?"
