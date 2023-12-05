e85ec3463380354462e692b44009e04fb469b6df
kumartanmay <ktanmay@gmail.com>
2023-12-05 04:37:48 +0000

    used an array to find the number at the given index of the fibonacci series

---
+++ LS/JS110/JS110-JS119/Revision/medium-1/7-fibonacci-number.js
@@ -0,0 +1,44 @@
+/*
+given the index of the number in the fibonacci series, return the number.
+
+ex
+==
+fibonacci(1);       // 1
+fibonacci(2);       // 1
+fibonacci(3);       // 2
+fibonacci(4);       // 3
+fibonacci(5);       // 5
+fibonacci(12);      // 144
+fibonacci(20);      // 6765
+
+ds:
+==
+input: number (index of the number in series)
+output: number (number at the index of the series)
+
+algo:
+====
+1. at each recursion, check if the index of the number is same as the input
+2. if not, continue
+3. if yes, return the number
+*/
+
+function fibonacci(index) {
+    let fiboSeries = [1, 1];
+    
+    if(index < 3) return 1;
+    
+    for(let count = 3; count <= index; count++) {
+        fiboSeries[count - 1] = fiboSeries[count - 2] + fiboSeries[count - 3]
+    }
+    
+    return fiboSeries[fiboSeries.length-1];
+}
+
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(4));
+console.log(fibonacci(5));
+console.log(fibonacci(12));
+console.log(fibonacci(20));
