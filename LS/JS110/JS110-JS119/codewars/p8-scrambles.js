b4044661db9f7bbea9a994dd211dbc4aec07aeea
kumartanmay <ktanmay@gmail.com>
2024-01-18 17:50:04 +0000

    added the problem statement

--- LS/JS110/JS110-JS119/codewars/p8-scrambles.js
+++ LS/JS110/JS110-JS119/codewars/p8-scrambles.js
@@ -1,3 +1,21 @@
+/*
+Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, 
+otherwise returns false.
+
+Notes:
+
+    Only lower case letters will be used (a-z). No punctuation or digits will be included.
+    Performance needs to be considered.
+
+Examples
+
+scramble('rkqodlw', 'world') ==> True
+scramble('cedewaraaossoqqyt', 'codewars') ==> True
+scramble('katas', 'steak') ==> False
+
+
+*/
+
 function scramble(str1, str2) {
   /*
   check if letters of str2 exists in str1,
